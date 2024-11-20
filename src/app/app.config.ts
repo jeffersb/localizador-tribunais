d6222c071e57e1f468e217e0de86fe225ab02e14
Jefferson Borges da Silva Moreira <jeffersb@stj.jus.br>
2024-11-20 11:37:45 -0300

    valdiacao formulario

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { MessageService } from 'primeng/api';
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideRouter(routes),
         provideHttpClient(),
         importProvidersFrom(BrowserAnimationsModule),
+        importProvidersFrom(MessageService),
     ],
 };
--- src/app/pages/tribunal-detalhe/tribunal-detalhe.component.ts
+++ src/app/pages/tribunal-detalhe/tribunal-detalhe.component.ts
@@ -4,6 +4,9 @@ import { ContainerComponent } from '../../layout/container/container.component';
 import { TribunalModel } from '../../models/tribunal.model';
 import { TribunaisService } from '../../services/tribunais.service';
 import { ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MessageService, PrimeNGConfig } from 'primeng/api';
+import { ToastModule } from 'primeng/toast';
 
 @Component({
     selector: 'app-tribunal-detalhe',
@@ -12,29 +15,60 @@ import { ActivatedRoute } from '@angular/router';
     styleUrl: './tribunal-detalhe.component.css',
     imports: [
         CommonModule,
-        ContainerComponent
+        ContainerComponent,
+        ToastModule,
+        ReactiveFormsModule
     ],
+    providers: [MessageService]
 })
 export class TribunalDetalheComponent implements OnInit, OnDestroy {
 
-    tribunaisService = inject(TribunaisService);
 
     codigo!:string;
     tribunal! : any;
+    contatoForm: FormGroup;
 
-    constructor(private activatedRoute: ActivatedRoute){
+    constructor(private activatedRoute: ActivatedRoute, 
+        private primeNgConfig: PrimeNGConfig,
+        private tribunaisService: TribunaisService,
+        private messageService: MessageService,
+        private fb: FormBuilder){
         this.activatedRoute.params.subscribe(params=>{
            this.codigo = params?.['codigo'];
         }); 
+       
+        this.contatoForm = this.fb.group({
+            nome: ['', Validators.required],
+            email: ['', [Validators.required, Validators.email]],
+            msg: ['', [Validators.required]]
+          });
      }
      
-    
     
     ngOnInit(): void {
         this.tribunaisService.getTribunais().subscribe(data=>{
             this.tribunal = data.find(el=>el.codigo===this.codigo)
         })
+        this.primeNgConfig.ripple = true;
     }
+
+    onSubmit() {
+        if (this.contatoForm.valid) {
+            console.log("ok");
+            this.messageService.add({ severity: 'success', summary: 'Successo', detail: 'Mensagem enviada com sucesso!' })
+        }
+      }
+
+      toggleValidation() {
+        const control = this.contatoForm.get('nome');
+        if (control!.validator === Validators.required) {
+          control!.clearValidators();
+        } else {
+          control!.setValidators(Validators.required);
+        }
+        control!.updateValueAndValidity();
+      }
+
 
     ngOnDestroy(): void {
        
